2e7c391c3f3bda4d63ced2c23adb03c882e47612
Антон <toni.golovkin.89@inbox.ru>
2021-10-21 19:06:16 +0300

    gb.chat1

---
+++ chat-client/src/main/java/client/Lesson4.java
@@ -0,0 +1,62 @@
+package client;
+
+public class Lesson4 {
+    static Object obj = new Object();
+    static volatile char controlVar = 'A';
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (obj) {
+                        while (controlVar != 'A') {
+                            obj.wait();
+                        }
+                        System.out.print("A ");
+                        controlVar = 'B';
+                        obj.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (obj) {
+                        while (controlVar != 'B') {
+                            obj.wait();
+                        }
+                        System.out.print("B ");
+                        controlVar = 'C';
+                        obj.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    synchronized (obj) {
+                        while (controlVar != 'C') {
+                            obj.wait();
+                        }
+                        System.out.print("C ");
+                        controlVar = 'A';
+                        obj.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
+/*
+По второму заданию думаю нет смысла добалять ExecutorService.
+Он скорее всего здесь понадобился бы если мы передавали друг другу какие то тяжелые файлы одновременно
+(аудио сообщения, фото, видео)
+ */
